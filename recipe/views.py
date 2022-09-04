4e85fbd77db0f948e89c481af3187bbf95a43dde
ssanger <blulady@gmail.com>
2022-09-03 21:48:17 -0700

    added templates, urls and views for recipe detail/list views. list view is working detail is not

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Diet, Ingredients, Recipe
+from django.views import generic
 
-# Create your views here.
+
+def index(request):
+    """view function for home page of site"""
+    recipe_count = Recipe.objects.all().count()
+    diet_count = Diet.objects.all().count()
+
+    context = {
+        'recipe_count': recipe_count,
+        'diet_count': diet_count
+    }
+
+    return render(request, 'index.html', context=context)
+
+
+class RecipeListView(generic.ListView):
+    model = Recipe
+
+
+class RecipeDetailView(generic.DetailView):
+    model = Recipe
--- recipes/urls.py
+++ recipes/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# from django.views.generic import RedirectView #would you like to redirect the url to recipe?
+from django.views.generic import RedirectView  # would you like to redirect the url to recipe?
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("recipe/", include("recipe.urls"))
-    # path("", RedirectView.as_view(url="recipe/", permanent=True)),
+    path("recipe/", include("recipe.urls")),
+    path("", RedirectView.as_view(url="recipe/", permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
